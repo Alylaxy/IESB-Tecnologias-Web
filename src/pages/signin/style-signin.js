cb5d90be1de914660f19d4d4e0a8bde9a4a663a3
MarceloCPJunior <53942899+MarceloCPJunior@users.noreply.github.com>
2023-05-02 19:28:27 -0300

    Update style-signin.js

--- src/pages/signin/style-signin.js
+++ src/pages/signin/style-signin.js
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const Container = styled.div`
   flex-direction: row;
   width: 1080px;
-  margin: 0 auto;
+  margin-top: 40%;
+  margin-left: 40%;
 `;
